7cf9de53f06ae93e09eb97f108822294718b112b
Rohit Sehgal <rohitben@hotmail.com>
2015-04-21 14:38:55 -0500

    Update recursiveArrays.js

--- recursiveArrays.js
+++ recursiveArrays.js
@@ -4,23 +4,22 @@ function buildArray(arrayIn, intensity){
   }else{
     arrayIn.push([]);
     for(var i = 0; i < arrayIn.length; i++){
-      arrayIn[i].push([Math.round(Math.random()*100), Math.round(Math.random()*100)]);
+      arrayIn[i].push([Math.round(Math.random()*1000), Math.round(Math.random()*1000)],[Math.round(Math.random()*1000), Math.round(Math.random()*1000)]);
     }
     return buildArray(arrayIn, intensity);
   }
-  
 }
 function drawCanvas(arrayIn){
   var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
   ctx.beginPath();
   for(var i = 0; i < arrayIn.length; i++){
-    for(var j = 0; j < arrayIn[i].length; j++){
+    for(var j = 0; j < arrayIn[i].length-1; j++){
+      console.log(arrayIn[i]);
       console.log(arrayIn[i][j]);
-      ctx.moveTo(0, 0);
-      ctx.lineTo(arrayIn[i][j][0], arrayIn[i][j][1]);
+      ctx.moveTo(arrayIn[i][j][0], arrayIn[i][j][1]);
+      ctx.lineTo(arrayIn[i][j+1][0], arrayIn[i][j+1][1]);
     }
   }
-  
   ctx.stroke();
 }
