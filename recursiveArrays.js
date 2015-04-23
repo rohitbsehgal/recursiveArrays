fae28961291449012624701722cc1af9db24b06c
Rohit Sehgal <rohitben@hotmail.com>
2015-04-23 13:38:18 -0500

    Update recursiveArrays.js

--- recursiveArrays.js
+++ recursiveArrays.js
@@ -21,31 +21,7 @@ function drawCanvas(arrayIn){
       ctx.lineTo(arrayIn[i][j+1][0], arrayIn[i][j+1][1]);
     }
   }
-  var w = Math.random();
-  var x = Math.random();
-  var y = Math.random();
-  var z = Math.random();
-  if(w < 0.5){
-    w = 0;
-  }else{
-    w = 500;
-  }
-  if(x < 0.5){
-    x = 0;
-  }else{
-    x = 500;
-  }
-  if(w == 0){
-    y = 500;
-  }else{
-    y = 0;
-  }
-  if(x == 0){
-    z = 500;
-  }else{
-    z = 0;
-  }
-  var grd = ctx.createLinearGradient(w, x, y, z);
+  var grd = ctx.createLinearGradient(0,0,500,0);
   grd.addColorStop(0, "red");
   grd.addColorStop(0.25, "green");
   grd.addColorStop(0.5, "blue");
