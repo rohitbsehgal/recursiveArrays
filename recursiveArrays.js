55c7ddab64ddd1d96c733daba80137cf8c6c58b7
Rohit Sehgal <rohitben@hotmail.com>
2015-04-23 14:11:20 -0500

    Update recursiveArrays.js

--- recursiveArrays.js
+++ recursiveArrays.js
@@ -19,16 +19,17 @@ function drawCanvas(arrayIn){
       console.log(arrayIn[i][j]);
       ctx.moveTo(arrayIn[i][j][0], arrayIn[i][j][1]);
       ctx.lineTo(arrayIn[i][j+1][0], arrayIn[i][j+1][1]);
+      var grd = ctx.createLinearGradient(0,0,500,0);
+      grd.addColorStop(0, "red");
+      grd.addColorStop(0.25, "green");
+      grd.addColorStop(0.5, "blue");
+      grd.addColorStop(0.75, "yellow");
+      grd.addColorStop(1, "orange");
+      ctx.strokeStyle=grd;
+      ctx.stroke();
     }
   }
-  var grd = ctx.createLinearGradient(0,0,500,0);
-  grd.addColorStop(0, "red");
-  grd.addColorStop(0.25, "green");
-  grd.addColorStop(0.5, "blue");
-  grd.addColorStop(0.75, "yellow");
-  grd.addColorStop(1, "orange");
-  ctx.strokeStyle=grd;
-  ctx.stroke();
+  
 }
 function loopThru(times){
   for(var i = 0; i < times; i++){
